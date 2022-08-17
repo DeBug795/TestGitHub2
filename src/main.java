b06a9bc1774d6ae5ee953dc77955d371932f09a9
SyntaxError9886 <73121106+SyntaxError9886@users.noreply.github.com>
2022-08-16 22:26:15 -0400

    Added a new line just so I could commit and bush - RS

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String []args){
         System.out.println("Hello Wrld");
         System.out.println("I THINK IT WORKED");
+        System.out.println("i'm not sure if it worked :( ");
     }
 }
